558d1a605f3fd02ba8cd24bf3d00c871094d3e69
Igor Kostiakov <i.kostiakov@andersenlab.com>
2025-07-12 15:09:02 +0300

    Add environment variables

--- application/src/main/java/com/example/ec2/config/AwsConfig.java
+++ application/src/main/java/com/example/ec2/config/AwsConfig.java
@@ -14,10 +14,10 @@ import software.amazon.awssdk.services.sqs.SqsClient;
 @Configuration
 public class AwsConfig {
 
-    @Value("${aws.accessKeyId}")
+    @Value("${aws.access-key-id}")
     private String accessKeyId;
 
-    @Value("${aws.secretKey}")
+    @Value("${aws.secret-key}")
     private String secretKey;
 
     @Value("${aws.region}")
